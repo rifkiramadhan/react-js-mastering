d1b4ce597ca8ccf101289a4d5f29ebc2ea6e84ff
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-03-17 01:03:39 +0700

    Sending a training project to [React & Component Basics]

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toDateString()}</div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
