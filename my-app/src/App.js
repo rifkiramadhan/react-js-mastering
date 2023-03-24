d44312bbbf88a1bac1cbc50bd80651ff07ad33a2
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-03-24 15:32:42 +0700

    Sending a training project to [Time to Practice: React & Component Basics]

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import './App.css';
+import Expenses from './components/Expenses';
 
-const App = () => {
+function App() {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 2, 14),
     },
     {
       id: 'e2',
       title: 'New TV',
       amount: 799.49,
-      date: new Date(2021, 2, 12),
+      date: new Date(2021, 3, 12),
     },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 4, 28),
     },
     {
       id: 'e4',
@@ -30,25 +29,12 @@ const App = () => {
     },
   ];
 
-  const addExpenseHandler = (expenses) => {
-    console.log('In App.js');
-    console.log(expenses);
-  };
-
-  // Menggukan Method React.CreateElement / Tidak menggukan JSX Code
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
+      <h2>Let's get started</h2>
       <Expenses items={expenses} />
     </div>
   );
-};
+}
 
 export default App;
--- my-app/src/components/ExpenseDate.js
+++ my-app/src/components/ExpenseDate.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import './ExpenseDate.css';
 
-const ExpenseDate = (props) => {
+function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
@@ -13,6 +12,6 @@ const ExpenseDate = (props) => {
       <div className='expense-date__day'>{day}</div>
     </div>
   );
-};
+}
 
 export default ExpenseDate;
