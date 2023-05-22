d91d02af7f36dab028bb2345baf1ab741d405fee
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-05-22 16:00:59 +0700

    Sending a training project to [Managing Cart & Modal State]

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCardButton />
+        <HeaderCardButton onClick={props.onShowCart} />
       </header>
 
       <div className={classes['main-image']}>
--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
@@ -30,12 +32,22 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{mealList}</ul>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
   );
 };
